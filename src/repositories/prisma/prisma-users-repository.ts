f8d55d6eead45208635782c05bcda5dc44792b79
JonasDuarte18 <the.jonasduarte@proton.me>
2025-12-29 16:26:48 -0300

    Adicionar o caso de uso GetUserProfile e os testes relacionados; atualizar o README com os requisitos concluídos.

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,18 @@
 import { prisma } from '@/lib/prisma'
-import { type Prisma } from '@/../prisma/prisma-client/client'
+import { type Prisma, type User } from '@/../prisma/prisma-client/client'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
